e50f2ef15600e7fb1d68ef0b7cd22949aaf23d8e
jithendiran27 <jithendiran7@gmail.com>
2023-09-28 17:53:18 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+// import { ToolBarComponent } from './tool-bar/tool-bar.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
